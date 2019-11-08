0d366b5aedab60c315fefb68d1db004db49b91dc
pablovizard <pabloferreiraufv@gmail.com>
2019-11-08 12:05:51 -0300

    modificações nos sprites

--- Assets/Scripts/PlayerMoviment2.cs
+++ Assets/Scripts/PlayerMoviment2.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Rewired;
 
 public class PlayerMoviment2 : MonoBehaviour
 {
-    public Animator animator;
+    public int playerId;
+    //Player player;
+    public Vector2 movementDirection;
     public float velocidade;
+    public float MOVEMENT_BASE_SPEED = 1.0f;
+    public Animator animator;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-        animator.SetFloat("X", movimento.x);
-        animator.SetFloat("Y", movimento.y);
-        animator.SetFloat("velocidade", movimento.magnitude);
 
-        transform.position = transform.position + movimento * velocidade * Time.deltaTime;
+        ProcessInputs();
+        Move();
+        Animate();
+    }
+    void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        velocidade = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+    void Move()
+    {
+        rb.velocity = movementDirection * velocidade * MOVEMENT_BASE_SPEED;
+    }
+        
+    void Animate()
+    {
+        if(movementDirection != Vector2.zero)
+        {
+            animator.SetFloat("X", movementDirection.x);
+            animator.SetFloat("Y", movementDirection.y);
+        }
+        animator.SetFloat("speed", velocidade);
     }
 }
