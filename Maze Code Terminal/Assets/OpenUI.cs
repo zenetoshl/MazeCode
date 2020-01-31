588b4f460153f4d71564c62315642e30d4ac07e1
Jose Neto <zenetoshl@gmail.com>
2020-01-31 08:03:12 -0300

    mudanças no drag e no click para UI

--- Maze Code Terminal/Assets/OpenUI.cs
+++ Maze Code Terminal/Assets/OpenUI.cs
@@ -1,17 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Lean.Gui;
+
 
 public class OpenUI : MonoBehaviour
 {
-    public LeanWindow thiswindow;
+    
     // Start is called before the first frame update
-    private void OnMouseUpAsButton()
-    {
-        if (!ClickController.isClickingOnObject)
-        {
-            thiswindow.TurnOn();
-        }
-    }
+    
 }
--- Maze Code Terminal/Assets/Scripts/Drag.cs
+++ Maze Code Terminal/Assets/Scripts/Drag.cs
@@ -1,25 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Lean.Gui;
 
-public class Drag : MonoBehaviour {
-    // Start is called before the first frame update
+public class Drag : MonoBehaviour
+{
+    private LeanWindow thisWindow;
     private Vector3 screenPoint;
     private Vector3 offset;
-    
-    void OnMouseDown () {
+    private int holdFrameCount;
+    public int minFrameCount = 30;
+    private bool drag;
+    private bool changed;
+
+    void OnMouseDown()
+    {
         ClickController.isClickingOnObject = true;
-        screenPoint = Camera.main.WorldToScreenPoint (transform.position);
-        offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
-    void OnMouseDrag () {
-        Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
-        transform.position = curPosition;
+    void OnMouseDrag()
+    {
+        if (drag)
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+        }
+
     }
 
-    void OnMouseUp() {
+    private void OnMouseUp()
+    {
+        changed = false;
+        holdFrameCount = 0;
         ClickController.isClickingOnObject = false;
+        drag = false;
+    }
+
+    private void Start()
+    {
+        holdFrameCount = 0;
+        drag = false;
+        changed = false;
+        thisWindow = this.GetComponentInChildren<LeanWindow>();
+    }
+
+    private void OnMouseOver() {
+        if (Input.GetMouseButton(0))
+        {
+            if (!drag)
+            {
+                holdFrameCount++;
+                if (!changed)
+                {
+                    changed = true;
+                }
+            }
+
+        }
+    }
+
+
+    private void OnMouseUpAsButton()
+    {
+        if (!drag)
+        {
+            thisWindow.TurnOn();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (holdFrameCount > minFrameCount && changed)
+        {
+            drag = true;
+            changed = false;
+        }
     }
 }
