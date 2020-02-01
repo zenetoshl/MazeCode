7b2cda0f0708a8a3472d3f74124766609402cd76
Jose Neto <zenetoshl@gmail.com>
2020-02-01 12:15:14 -0300

    update exemplo interface

---
+++ Maze Code Terminal/Assets/ToggleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class ToggleManager : MonoBehaviour
+{
+    public GameObject dropdown;
+    public GameObject textInput;
+    private TMP_Dropdown TMPdropdown;
+    private TMP_InputField TMPinput;
+
+    private void Start() {
+        TMPdropdown = dropdown.GetComponent<TMP_Dropdown>();
+        TMPinput = textInput.GetComponent<TMP_InputField>();
+    }
+
+    public void toggle(){
+        dropdown.SetActive(!TMPdropdown.interactable);
+        textInput.SetActive(!TMPinput.interactable);
+        TMPinput.interactable = !TMPinput.interactable;
+        TMPdropdown.interactable = !TMPdropdown.interactable;    
+    }
+}
