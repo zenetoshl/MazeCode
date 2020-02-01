b46a9e489b5c8905fc3d36254159eae7a53bfb58
Jose Neto <zenetoshl@gmail.com>
2020-02-01 17:55:25 -0300

    conexao interface com texto

--- Maze Code Terminal/Assets/Scripts/Blocos/BlocoFor.cs
+++ Maze Code Terminal/Assets/Scripts/Blocos/BlocoFor.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BlocoFor : Bloco
 {
-    public int Increment;
-        public int Begin;
-        public string EndOp;
+        public TextMeshProUGUI boolean;
+        public TextMeshProUGUI increment;
+        public TextMeshProUGUI variable1;
+        public TextMeshProUGUI booleanOp;
+        public TextMeshProUGUI variable2Dropdown;
+        public TextMeshProUGUI variable2Text;
+        public TextMeshProUGUI mathOp;
+        public TextMeshProUGUI incrementValue;
+        public Toggle toggle;
+        private string forIncrement;
+        private string forBegin;
+    
 
         public override string ToCode()
         {
-            string BlocoCode = "for(int i =" + Begin +";"+ EndOp + "; i = i +(" + Increment + "))";
+            string BlocoCode = "for( "+ forBegin +";"+ boolean.text + ";" + forIncrement + ")";
             return BlocoCode;
+        }
+
+        public void onWindowOff(){
+            string aux = toggle.isOn ? variable2Dropdown.text : variable2Text.text;
+            boolean.text =  variable1.text + " " + booleanOp.text + " " + aux;
+            increment.text =  mathOp.text + incrementValue.text;
+            forIncrement = variable1.text + " = " + variable1.text + mathOp.text + incrementValue.text;
+            forBegin = "int " + variable1.text + " = " + "0"; 
+
         }
 }
