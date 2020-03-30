13a8011c6670be1bbefae05c0cdcc204c0799464
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-03-30 15:49:06 -0300

    Remoção de pastas do Code

--- Code/Assets/Scripts/ClickController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ClickController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public static bool isClickingOnObject = false; 
-}
