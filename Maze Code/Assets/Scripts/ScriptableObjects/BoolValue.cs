113dd4f9865186a97d9f102696f1b4240d7c6879
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-04-30 21:54:22 -0300

    Persistencia de dados implementada

--- Maze Code/Assets/Scripts/ScriptableObjects/BoolValue.cs
+++ Maze Code/Assets/Scripts/ScriptableObjects/BoolValue.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
+[System.Serializable]
 public class BoolValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public bool initialValue;
 
-    [HideInInspector]
     public bool RuntimeValue;
 
-    public void OnAfterDeserialize()
-    {
-        RuntimeValue = initialValue;
-    }
+    public void OnAfterDeserialize() { }
 
     public void OnBeforeSerialize() { }
 }
