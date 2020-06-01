7855cff809e907d7ea53a5c1079771769a975dc3
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-05-31 21:15:22 -0300

    Revert "Teste"
    This reverts commit 3a8d35ade58b71efbb5bcd20d95b648acfe3f262.

--- Maze Code/Assets/Maze/Scripts/PauseManager.cs
+++ Maze Code/Assets/Maze/Scripts/PauseManager.cs
@@ -5,33 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
-    protected JoyButtonInventory joybutton;
-
     private bool isPaused;
+    private bool isInventory;
+
     public GameObject pausePanel;
     public GameObject inventoryPanel;
-    public bool usingPausePanel; 
+    
     public string mainMenu;
     
     // Start is called before the first frame update
     void Start()
     {
-        joybutton = FindObjectOfType<JoyButtonInventory>();
-        
         isPaused = false;
+        isInventory = false;
         pausePanel.SetActive(false);
         inventoryPanel.SetActive(false);
-        usingPausePanel = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(joybutton.Pressed)
-        {
-            ChangePause();
-        }
-        
+
     }
 
     public void ChangePause()
@@ -41,12 +35,25 @@ public class PauseManager : MonoBehaviour
         {
             pausePanel.SetActive(true);
             Time.timeScale = 0f;
-            usingPausePanel = true;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void ChangeInventory()
+    {
+        isInventory = !isInventory;
+        if (isInventory)
+        {
+            inventoryPanel.SetActive(true);
+            Time.timeScale = 0f;
         }
         else
         {
             inventoryPanel.SetActive(false);
-            pausePanel.SetActive(false);
             Time.timeScale = 1f;
         }
     }
@@ -55,19 +62,5 @@ public class PauseManager : MonoBehaviour
     {
         SceneManager.LoadScene(mainMenu);
         Time.timeScale = 1f;
-    }
-
-    public void SwitchPanels()
-    {
-        usingPausePanel = !usingPausePanel;
-        if(usingPausePanel)
-        {
-            pausePanel.SetActive(true);
-            inventoryPanel.SetActive(false);
-        }
-        else {
-            pausePanel.SetActive(false);
-            inventoryPanel.SetActive(true);
-        }
     }
 }
