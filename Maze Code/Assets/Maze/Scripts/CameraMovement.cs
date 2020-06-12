500e7ee7173a3a0b14a8eee4e87690cb88a29bb2
Jose Neto <zenetoshl@gmail.com>
2020-06-12 09:40:45 -0300

    atualizando cameramovement

--- Maze Code/Assets/Maze/Scripts/CameraMovement.cs
+++ Maze Code/Assets/Maze/Scripts/CameraMovement.cs
@@ -2,38 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
+public class CameraMovement : MonoBehaviour {
 
-    [Header("Position Variables")]
+    [Header ("Position Variables")]
     public Transform target;
     public float smoothing;
     public Vector2 maxPosition;
     public Vector2 minPosition;
 
-    [Header("Position Reset")]
+    private Vector2 maxPositionMap;
+    private Vector2 minPositionMap;
+
+    [Header ("Position Reset")]
     public VectorValue camMin;
     public VectorValue camMax;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        maxPosition = camMax.initialValue;
-        minPosition = camMin.initialValue;
+    void Start () {
+        //maxPosition = camMax.initialValue;
+        //minPosition = camMin.initialValue;
+        float vertExtent = Camera.main.GetComponent<Camera> ().orthographicSize;
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+        Debug.Log (vertExtent);
+        Debug.Log (horzExtent);
 
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);   
+        minPositionMap.x = (minPosition.x + (horzExtent));
+        maxPositionMap.x = (maxPosition.x - (horzExtent));
+        minPositionMap.y = (minPosition.y + (vertExtent));
+        maxPositionMap.y = (maxPosition.y - (vertExtent));
+
+
+        Debug.Log (minPositionMap);
+        Debug.Log (maxPositionMap);
+        transform.position = new Vector3 (target.position.x, target.position.y, transform.position.z);
     }
 
     // Update is called once per frame
-    void LateUpdate()
-    {
-        if(transform.position != target.position)
-        {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+    void LateUpdate () {
+        if (transform.position != target.position) {
+            Vector3 targetPosition = new Vector3 (target.position.x, target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp (targetPosition.x, minPositionMap.x, maxPositionMap.x);
+            targetPosition.y = Mathf.Clamp (targetPosition.y, minPositionMap.y, maxPositionMap.y);
+            transform.position = Vector3.Lerp (transform.position, targetPosition, smoothing);
         }
-        maxPosition = target.position;
     }
 }
