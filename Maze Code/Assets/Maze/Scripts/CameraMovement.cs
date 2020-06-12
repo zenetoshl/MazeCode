dc092b36fec49bf50ba019d50c5b496c9f71c8da
Jose Neto <zenetoshl@gmail.com>
2020-06-12 16:26:39 -0300

    salvando camera e posição do persnagem

--- Maze Code/Assets/Maze/Scripts/CameraMovement.cs
+++ Maze Code/Assets/Maze/Scripts/CameraMovement.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
@@ -19,19 +21,20 @@ public class CameraMovement : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start () {
-        maxPositionMap = camMax.initialValue;
-        minPositionMap = camMin.initialValue;
+
+        LoadLimits ();
+        CalculateLimits ();
+
+        transform.position = new Vector3 (target.position.x, target.position.y, transform.position.z);
+    }
+
+    void CalculateLimits () {
         float vertExtent = Camera.main.GetComponent<Camera> ().orthographicSize;
         float horzExtent = vertExtent * Screen.width / Screen.height;
-        Debug.Log (vertExtent);
-        Debug.Log (horzExtent);
-
         minPosition.x = (minPositionMap.x + (horzExtent));
         maxPosition.x = (maxPositionMap.x - (horzExtent));
         minPosition.y = (minPositionMap.y + (vertExtent));
         maxPosition.y = (maxPositionMap.y - (vertExtent));
-
-        transform.position = new Vector3 (target.position.x, target.position.y, transform.position.z);
     }
 
     // Update is called once per frame
@@ -43,9 +46,46 @@ public class CameraMovement : MonoBehaviour {
             transform.position = Vector3.Lerp (transform.position, targetPosition, smoothing);
         }
     }
-    void OnApplicationQuit()
-    {
-        //maxPosition.x = Current;
-        //maxPosition.y = Current;
+
+    public void SaveLimits () {
+        FileStream fileMax = File.Create (Application.persistentDataPath + "/max.cam");
+        FileStream fileMin = File.Create (Application.persistentDataPath + "/min.cam");
+        BinaryFormatter binary = new BinaryFormatter ();
+        var jsonMax = JsonUtility.ToJson (maxPositionMap);
+        var jsonMin = JsonUtility.ToJson (minPositionMap);
+        binary.Serialize (fileMax, jsonMax);
+        binary.Serialize (fileMin, jsonMin);
+        fileMax.Close ();
+        fileMin.Close ();
+
+    }
+
+    public void LoadLimits () {
+        if (File.Exists (Application.persistentDataPath + "/min.cam")) {
+            FileStream file = File.Open (Application.persistentDataPath + "/min.cam", FileMode.Open);
+            BinaryFormatter binary = new BinaryFormatter ();
+            minPositionMap = JsonUtility.FromJson<Vector2> ((string) binary.Deserialize (file));
+            file.Close ();
+        }
+        if (File.Exists (Application.persistentDataPath + "/max.cam")) {
+            FileStream file = File.Open (Application.persistentDataPath + "/max.cam", FileMode.Open);
+            BinaryFormatter binary = new BinaryFormatter ();
+            maxPositionMap = JsonUtility.FromJson<Vector2> ((string) binary.Deserialize (file));
+            file.Close ();
+        }
+
+    }
+
+    private void OnDisable () {
+        SaveLimits ();
+    }
+
+    private void OnEnable () {
+        LoadLimits ();
+        CalculateLimits ();
+    }
+
+    private void OnApplicationQuit() {
+        SaveLimits ();
     }
 }
