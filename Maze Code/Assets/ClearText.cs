10bd6a9386941432ef71ed23ce7ef91d026d90ee
Jose Neto <zenetoshl@gmail.com>
2020-06-19 12:03:40 -0300

    pintando janelas do terminal

---
+++ Maze Code/Assets/ClearText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClearText : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    public void ClearThisText(){
+        text.text = "";
+    }
+}
