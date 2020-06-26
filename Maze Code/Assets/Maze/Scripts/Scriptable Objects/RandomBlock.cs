a38a2ab15b47a91030735e8c570d7d9726836e32
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-06-25 22:50:18 -0300

    Definição de areas aleatorias

--- Maze Code/Assets/Maze/Scripts/Scriptable Objects/RandomBlock.cs
+++ Maze Code/Assets/Maze/Scripts/Scriptable Objects/RandomBlock.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 [CreateAssetMenu]
 public class RandomBlock : ScriptableObject
 {
-    [Header("Número da sala do Random")]
+    [Header("Random space da sala")]
     public int numSala;
 
-    [Header("Centralização da área de geração")]
+    [Header("Blocos que a sala possui")]
+    public int variavel;        // 1 - Vermelho
+    public int leitura;         // 2 - Laranja
+    public int imprime;         // 3 - Amarelo
+    public int matematica;      // 4 - Verde
+    public int condicional;     // 5 - Azul
+    public int lacoDefinido;    // 6 - Roxo
+    public int lacoIndefinido;  // 7 - Rosa
+    public int vetor;           // 8 - Marrom
+    public int matriz;          // 9 - Cinza
+
+    [Header("Posição da área de geração")]
     public Vector2 position;
 
     [Header("Tamanho da área")]
