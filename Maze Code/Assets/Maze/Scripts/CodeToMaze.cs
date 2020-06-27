1663ecd60d899354973332dd115d5718cce95587
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-06-27 17:38:15 -0300

    Incremento do bonus (Terminal)

--- Maze Code/Assets/Maze/Scripts/CodeToMaze.cs
+++ Maze Code/Assets/Maze/Scripts/CodeToMaze.cs
@@ -17,6 +17,9 @@ public class CodeToMaze : MonoBehaviour
     [Header("Puzzle Status")]
     public Puzzle puzzleStatus;
 
+    [Header("Incrementar Bonus")]
+    public PlayerInventory inventory = null;
+
     public void Awake()
     {
         if(fadeInPanel != null)
@@ -31,7 +34,25 @@ public class CodeToMaze : MonoBehaviour
         // O jogador conseguiu realizar o desafio e destravar a porta
         //puzzleStatus.runtimeValue = true;
 
-        // TODO: Incrementar o invetário com o bonus do puzzle
+        // Incrementar o invetário com o bonus do puzzle
+        // "0 - Variaveis"        
+        inventory.myInventory[0].numberHeld = inventory.myInventory[0].numberHeld + puzzleStatus.bonusVariavel;
+        // "1 - Leitura"
+        inventory.myInventory[1].numberHeld = inventory.myInventory[1].numberHeld + puzzleStatus.bonusLeitura;
+        // "2 - Imprime"
+        inventory.myInventory[2].numberHeld = inventory.myInventory[2].numberHeld + puzzleStatus.bonusImprime;
+        // "3 - Matematica"
+        inventory.myInventory[3].numberHeld = inventory.myInventory[3].numberHeld + puzzleStatus.bonusMatematica;
+        // "4 - Condicional"
+        inventory.myInventory[4].numberHeld = inventory.myInventory[4].numberHeld + puzzleStatus.bonusCondicional;
+        // "5 - Loop For"
+        inventory.myInventory[5].numberHeld = inventory.myInventory[5].numberHeld + puzzleStatus.bonusLoopDefinido;
+        // "6 - Loop While"
+        inventory.myInventory[6].numberHeld = inventory.myInventory[6].numberHeld + puzzleStatus.bonusLoopIndefinido;
+        // "7 - Vetor"
+        inventory.myInventory[7].numberHeld = inventory.myInventory[7].numberHeld + puzzleStatus.bonusVetor;
+        // "8 - Matriz"
+        inventory.myInventory[8].numberHeld = inventory.myInventory[8].numberHeld + puzzleStatus.bonusMatriz;
 
         StartCoroutine(FadeControl());
     }
