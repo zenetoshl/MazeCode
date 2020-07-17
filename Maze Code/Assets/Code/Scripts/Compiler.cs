742bbf180d403d3c4ae254d16b0518be5a8ffa38
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-07-17 13:50:36 -0300

    Teste de botões

--- Maze Code/Assets/Code/Scripts/Compiler.cs
+++ Maze Code/Assets/Code/Scripts/Compiler.cs
@@ -6,21 +6,21 @@ using Lean.Gui;
 public class Compiler : MonoBehaviour
 {
     public Transform inicial;
-    public LeanButton executar;
-    public LeanButton enviar;
+    public GameObject executar;
+    public GameObject enviar;
 
     public void Compile(){
         if(ConnectionManager.Compile(inicial.GetComponent<RectTransform>())){
-            executar.interactable = true;
-            enviar.interactable = true;
+            executar.SetActive (true);
+            enviar.SetActive (true);
         }else{
-            executar.interactable = false;
-            enviar.interactable = false;
+            executar.SetActive (false);
+            enviar.SetActive (false);
         }
     }
 
     public void Uncompile(){
-         executar.interactable = false;
-         enviar.interactable = false;
+        executar.SetActive (false);
+        enviar.SetActive (false);
     }
 }
