a00a8ff675efe9a518bb732ab085bf43d1a4798c
Pablo Ferreira <39393083+PabloVizard@users.noreply.github.com>
2020-07-17 13:19:01 -0300

    adicionado tela de loading

---
+++ Maze Code/Assets/Maze/Scripts/LoadingScreenControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenControl : MonoBehaviour
+{
+    public GameObject loadingScreenObj;
+    public Slider slider;
+    public int fakeSpeed;
+    AsyncOperation async;
+
+    // Start is called before the first frame update
+    public void LoadScreenExample()
+    {
+        StartCoroutine(LoadingScreen());
+    }
+    IEnumerator LoadingScreen()
+    {
+        loadingScreenObj.SetActive(true);
+        async = SceneManager.LoadSceneAsync(1);
+        async.allowSceneActivation = false;
+
+        while (async.isDone == false)
+        {
+            //slider.value += fakeSpeed * Time.deltaTime;
+            slider.value = async.progress * 0.9f;
+            if(async.progress == 0.9f)
+            {
+                slider.value = 1f;
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
