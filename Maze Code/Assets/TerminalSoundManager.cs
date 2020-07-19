bac53fbf1d1f08c856adfed67b97ed621958a3d4
Jose Neto <zenetoshl@gmail.com>
2020-07-19 19:29:57 -0300

    som terminal igual do labirinto

---
+++ Maze Code/Assets/TerminalSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalSoundManager : MonoBehaviour
+{
+
+    public SoundConfig soundConfig;
+    public AudioSource music;
+
+    private void Start() {
+        if(soundConfig.music){
+            StopMusic();
+        }
+    }
+
+    public void PlayMusic()
+    {
+        music.Play();
+    }
+    public void StopMusic()
+    {
+        music.Stop();
+    }
+}
