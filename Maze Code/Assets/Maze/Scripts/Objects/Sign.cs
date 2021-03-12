b56eba164e34e69e192a7d7c954235e84acdfb50
Pablo Ferreira <39393083+PabloVizard@users.noreply.github.com>
2021-03-12 12:16:40 -0300

    Alteração NPC

--- Maze Code/Assets/Maze/Scripts/Objects/Sign.cs
+++ Maze Code/Assets/Maze/Scripts/Objects/Sign.cs
@@ -17,7 +17,7 @@ public class Sign : Interactable
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(joybutton.Pressed && playerInRange)
         {
